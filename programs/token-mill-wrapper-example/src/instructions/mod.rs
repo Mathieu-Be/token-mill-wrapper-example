5a405e145d749bac124efb4b50a2adafc4e17fc0
Patate <patate@joepegs.com>
2025-01-31 11:10:12 +0100

    add `wrapped_buy_with_extra_fee`

--- programs/token-mill-wrapper-example/src/instructions/mod.rs
+++ programs/token-mill-wrapper-example/src/instructions/mod.rs
@@ -1,7 +1,9 @@
 pub mod graduate;
 pub mod initialize_wrapper_authority;
 pub mod simple_wrapped_swap;
+pub mod wrapped_buy_with_extra_fee;
 
 pub use graduate::*;
 pub use initialize_wrapper_authority::*;
 pub use simple_wrapped_swap::*;
+pub use wrapped_buy_with_extra_fee::*;
